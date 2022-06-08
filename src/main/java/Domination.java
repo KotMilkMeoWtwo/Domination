4e8bb3dde2230af0bfa57d226b84f64d13788178
kotmilkmeowtwo <forgithubkotmilkmeow@gmail.com>
2022-06-08 18:10:47 +0300

    Initial commit

---
+++ src/main/java/Domination.java
@@ -0,0 +1,11 @@
+import jdk.jpackage.internal.Log;
+import mindustry.mod.Plugin;
+
+public class Domination extends Plugin {
+
+
+    @Override
+    public void init(){
+        Log.info("meow");
+    }
+}
